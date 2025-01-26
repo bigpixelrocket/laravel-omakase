05b685a5dd32487b7d233de667d23d196f133db3
Lucian Văcăroiu <lucian@bigpixelrocket.com>
2025-01-26 18:26:29 +0200

    build: add tests and update deps
    add tests and update deps

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests;
+
+use Bigpixelrocket\LaravelOmakase\OmakaseServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            OmakaseServiceProvider::class,
+        ];
+    }
+
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadLaravelMigrations();
+    }
+}
