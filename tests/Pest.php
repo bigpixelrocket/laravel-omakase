1ec81d191cc4a291b3efd4b3fd68645f29acc3b3
Lucian Văcăroiu <lucian@bigpixelrocket.com>
2025-05-21 15:58:24 +0300

    docs: update guidelines and clean tests (#56)

--- tests/Pest.php
+++ tests/Pest.php
@@ -17,3 +17,15 @@ uses(Tests\TestCase::class)->in('Feature');
 expect()->extend('toBeOne', function ($value) {
     return expect($value)->toBe(1);
 });
+
+/**
+ * Call a protected method on an object using reflection.
+ */
+function callProtectedMethod(object $object, string $method, array $arguments = [])
+{
+    $reflection = new ReflectionClass($object);
+    $method = $reflection->getMethod($method);
+    $method->setAccessible(true);
+
+    return $method->invokeArgs($object, $arguments);
+}
