a0e540569841c79134d8ce08785d1bd379c08206
Lucian Văcăroiu <lucian@bigpixelrocket.com>
2025-07-05 12:09:47 +0300

    build: add rector workflow

--- rector.php
+++ rector.php
@@ -2,9 +2,14 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
+    ->withCache(
+        '/tmp/rector',
+        FileCacheStorage::class,
+    )
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/tests',
